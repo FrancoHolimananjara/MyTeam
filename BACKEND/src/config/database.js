ff2a4acf6b7d62f75acc2012e610f540db7eea26
Franco <andriamitantsoa36kofra@gmail.com>
2023-01-24 17:36:28 +0300

    edit database connection ,use only locally database for developpment

--- BACKEND/src/config/database.js
+++ BACKEND/src/config/database.js
@@ -5,20 +5,15 @@ const path = require('path');
 dotenv.config({ path: path.resolve(__dirname, '../../.env') });
 
 function connexion() {
+    mongoose.set('strictQuery', false);
+    // Here, we work only in locally database for developpmemt
     mongoose
-        .connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }, function (err) {
+        .connect(process.env.MONGODB_URI_LOCAL, { useNewUrlParser: true, useUnifiedTopology: true }, function (err) {
             if (err) {
-                mongoose
-                    .connect(process.env.MONGODB_URI_LOCAL, { useNewUrlParser: true, useUnifiedTopology: true }, function (err) {
-                        if (err) {
-                            console.log(err);
-                        }
-                        console.log("Connected - successfully - to " + mongoose.connection.host);
-                    });
-            } else {
-                console.log("Connected - successfully - to cluster " + mongoose.connection.host);
+                console.log(err);
             }
-        })
+            console.log("Connected - successfully - to " + mongoose.connection.host);
+        });
 }
 
 module.exports = connexion;
--- BACKEND/src/controllers/auth.controller.js
+++ BACKEND/src/controllers/auth.controller.js
@@ -10,7 +10,7 @@ module.exports = {
 
     // Valid credentials
     const newUser = await createUser({ username, email, password });
-    await sendEmailVerification(newUser);
+    // await sendEmailVerification(newUser);
     res.json(newUser);
   },
   verify_email: async (req, res) => {
