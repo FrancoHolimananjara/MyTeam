d80993e0c9d75b643836178cd5a00c96372559de
Franco <andriamitantsoa36kofra@gmail.com>
2023-02-03 06:35:33 +0300

    login form

--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -1,32 +1,42 @@
-import React from 'react';
+import React , { useState } from 'react';
 import './login.css';
 
 function Login() {
+
+    const [showPassword, setShowPassword] = useState(false);
+
     return (
         <section className="login section">
-            <div className="login__container container grid">
+            <div className="login__container grid">
                 <div className="login__content">
                     <form action="" className="login__form">
                         <h1>Login form</h1>
                         <div className="input__group">
-                            <label htmlFor="">Username</label>
-                            <input type="text" name="" id="" />
+                            <label htmlFor="username">Username</label>
+                            <input type="text" name="usename" id="username" />
                         </div>
                         <div className="input__group">
-                            <label htmlFor="">Username</label>
-                            <input type="text" name="" id="" />
+                            <div className="password">
+                                <label htmlFor="password">Password</label>
+                                <i className="bx bx-dizzy password__icon"></i>
+                            </div>
+                            <input type="password" name="" id="password" />
                         </div>
+                        <button className='button button__flex'>
+                            Sign In
+                        </button>
                         <p className="or">Or</p>
                         <div className="login__social">
                             <a href="/" className="button">
-                                <i className="bx bxl-google"></i> Google account
+                                <i className="bx bxl-google"></i> <u>Google</u>account
                             </a>
                             <a href="/" className="button">
-                                <i className="bx bxl-github"></i> GitHub account
+                                <i className="bx bxl-github"></i> <u>GitHub</u> account
                             </a>
                         </div>
                     </form>
                 </div>
+                <div className='login__img'></div>
             </div>
         </section>
     );
