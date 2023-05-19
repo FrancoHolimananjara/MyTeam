552571231f3b477d13d230ffae01a0b0ae72961b
FrancoHolimananjara <andriamitantsoa36kofra@gmail.com>
2023-05-19 22:02:26 +0300

    login page in progress

--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -1,61 +1,57 @@
-import React , { useState } from 'react';
-import './login.css';
-import axios from 'axios';
+import React, { useState } from "react";
+import "./login.css";
+import axios from "axios";
 
 function Login() {
+  const [showPassword, setShowPassword] = useState(false);
+  const [identifier, setIdentifier] = useState("");
+  const [password, setPassword] = useState("");
 
-    const [showPassword, setShowPassword] = useState(false);
-    const [identifier, setIdentifier] = useState("");
-    const [password, setPassword] = useState("");
+  const [token, setToken] = useState("");
 
-    const [token, setToken] = useState("");
-
-    const onSubmit = async () => {
-        const data = {
-            identifier,
-            password
-        }
-        const response = await axios.post("http://localhost:3001/api/auth/login", data);
-        setToken(response.data['token']);
-        localStorage.setItem('auth', response.data['token']);
-    }
-
-    return (
-        <section className="login section">
-            <div className="login__container">
-                <div className="login__content">
-                    <form action="" className="login__form">
-                        <div className="blank__info">
-                            <h1>Login form</h1>
-                            <div className="input__group">
-                                <label htmlFor="identifier">Identifier</label>
-                                <input type="text" name="usename" id="identifier" onChange={(e)=>{setIdentifier(e.target.value)}} />
-                            </div>
-                            <div className="input__group">
-                                <div className="password">
-                                    <label htmlFor="password">Password</label>
-                                    <i onClick={(e) => { setShowPassword(!showPassword);console.log(showPassword) }} className="bx bx-dizzy password__icon"></i>
-                                </div>
-                                <input type={showPassword ? 'text' : 'password'} name="" id="password" onChange={(e) => { setPassword(e.target.value) }}/>
-                            </div>
-                            <button type='button' className='button button__flex' onClick={()=>onSubmit()}>
-                                Sign In
-                            </button>
-                            <p className="or">Or</p>
-                        </div>
-                        <div className="login__social">
-                            <a href="/" className="button">
-                                <i className="bx bxl-google"></i> Google account
-                            </a>
-                            <a href="/" className="button">
-                                <i className="bx bxl-github"></i> GitHub account
-                            </a>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </section>
+  const onSubmit = async () => {
+    const data = {
+      identifier,
+      password,
+    };
+    const response = await axios.post(
+      "http://localhost:3001/api/auth/login",
+      data
     );
+    setToken(response.data["token"]);
+    localStorage.setItem("auth", response.data["token"]);
+  };
+
+  return (
+    <section className="login section">
+      <div className="login-box">
+        <h1 className="login-title">My Team</h1>
+        <form className="login-form">
+          <div className="identifier">
+            <label htmlFor="identifier">Identifier</label>
+            <input type="text" />
+          </div>
+          <div className="password">
+            <label htmlFor="password">Password</label>
+            <input type="password" />
+          </div>
+          <button className="login-signin">Sign In</button>
+          <p className="or">OR</p>
+          <div className="other-methode">
+            <button className="login-google">Google</button>
+            <button className="login-github">Github</button>
+          </div>
+          <p className="create-account">
+            Don't have account? click{" "}
+            <b>
+              <u>here</u>
+            </b>{" "}
+            to create one
+          </p>
+        </form>
+      </div>
+    </section>
+  );
 }
 
 export default Login;
