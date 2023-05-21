30f0916474cee693a9583b89370122d159c8e258
Franco <andriamitantsoa36kofra@gmail.com>
2023-05-21 19:01:00 +0300

    register action mi-work

--- client/src/components/register/register.jsx
+++ client/src/components/register/register.jsx
@@ -1,10 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import "../register/register.css";
 import FloatingLabel from "../../ui-kit/floating-label/FloatingLabel";
 import Button from "../../ui-kit/button/Button";
+import axios from "axios";
 
 function Register() {
-  const checkPassword = async (p, cp) => {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirm, setConfirm] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (checkPassword(password, confirm)) {
+      console.log(checkPassword(password, confirm));
+      axios
+        .post("http://localhost:3001/api/auth/register", {
+          username,
+          email,
+          password,
+        })
+        .then((response) => {
+          console.log(response);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    } else {
+      console.log("PASSWORD DOESN'T MATCH");
+    }
+  };
+
+  const checkPassword = (p, cp) => {
     return cp === p ? true : false;
   };
   return (
@@ -14,14 +41,26 @@ function Register() {
           My <i>Team</i>
         </h1>
         <form className="register-form">
-          <FloatingLabel name="Username" type="text" />
-          <FloatingLabel name="Email" type="email" />
+          <FloatingLabel name="Username" type="text" setState={setUsername} />
+          <FloatingLabel name="Email" type="email" setState={setEmail} />
           <div className="password-action">
-            <FloatingLabel name="Password" type="password" />
-            <FloatingLabel name="Confirm" type="password" />
+            <FloatingLabel
+              name="Password"
+              type="password"
+              setState={setPassword}
+            />
+            <FloatingLabel
+              name="Confirm"
+              type="password"
+              setState={setConfirm}
+            />
           </div>
           <div className="action-submit">
-            <Button name="Register" type="secondary" />
+            <Button
+              name="Register"
+              type="secondary"
+              handleSubmit={handleSubmit}
+            />
             <div className="choice">
               <p className="or">OR</p>
             </div>
